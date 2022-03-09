a967bba7f07d66a8009e65af53fdeab9fe2dfbb8
凌亦 <lingyi.zcs@alibaba-inc.com>
2022-03-09 20:12:39 +0800

    feat(react-scripts,create-react-app,cra-template-typescript): Modify version for raft, change default template to cra-template-typescript, remove checkForLatestVersion logic

--- packages/create-react-app/createReactApp.js
+++ packages/create-react-app/createReactApp.js
@@ -189,54 +189,15 @@ function init() {
     process.exit(1);
   }
 
-  // We first check the registry directly via the API, and if that fails, we try
-  // the slower `npm view [package] version` command.
-  //
-  // This is important for users in environments where direct access to npm is
-  // blocked by a firewall, and packages are provided exclusively via a private
-  // registry.
-  checkForLatestVersion()
-    .catch(() => {
-      try {
-        return execSync('npm view create-react-app version').toString().trim();
-      } catch (e) {
-        return null;
-      }
-    })
-    .then(latest => {
-      if (latest && semver.lt(packageJson.version, latest)) {
-        console.log();
-        console.error(
-          chalk.yellow(
-            `You are running \`create-react-app\` ${packageJson.version}, which is behind the latest release (${latest}).\n\n` +
-              'We no longer support global installation of Create React App.'
-          )
-        );
-        console.log();
-        console.log(
-          'Please remove any global installs with one of the following commands:\n' +
-            '- npm uninstall -g create-react-app\n' +
-            '- yarn global remove create-react-app'
-        );
-        console.log();
-        console.log(
-          'The latest instructions for creating a new app can be found here:\n' +
-            'https://create-react-app.dev/docs/getting-started/'
-        );
-        console.log();
-        process.exit(1);
-      } else {
-        const useYarn = isUsingYarn();
-        createApp(
-          projectName,
-          program.verbose,
-          program.scriptsVersion,
-          program.template,
-          useYarn,
-          program.usePnp
-        );
-      }
-    });
+  const useYarn = isUsingYarn();
+  createApp(
+    projectName,
+    program.verbose,
+    program.scriptsVersion,
+    program.template,
+    useYarn,
+    program.usePnp
+  );
 }
 
 function createApp(name, verbose, version, template, useYarn, usePnp) {
@@ -1098,29 +1059,6 @@ function executeNodeScript({ cwd, args }, data, source) {
       }
       resolve();
     });
-  });
-}
-
-function checkForLatestVersion() {
-  return new Promise((resolve, reject) => {
-    https
-      .get(
-        'https://registry.npmjs.org/-/package/create-react-app/dist-tags',
-        res => {
-          if (res.statusCode === 200) {
-            let body = '';
-            res.on('data', data => (body += data));
-            res.on('end', () => {
-              resolve(JSON.parse(body).latest);
-            });
-          } else {
-            reject();
-          }
-        }
-      )
-      .on('error', () => {
-        reject();
-      });
   });
 }
 
